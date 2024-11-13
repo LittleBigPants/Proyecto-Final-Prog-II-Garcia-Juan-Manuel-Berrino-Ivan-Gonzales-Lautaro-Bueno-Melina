01a0814a08f87c317aafb5908f6c3d0f5e992fcd
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2024-11-13 19:08:58 -0300

    Create validacion-registro.js

---
+++ website/static/js/validacion-registro.js
@@ -0,0 +1,48 @@
+const formulario = document.getElementById('formulario');
+const email = document.getElementById('email');
+const contraseña = document.getElementById('contraseña');
+const errorEmail = document.getElementById('errorEmail');
+const errorContraseña = document.getElementById('errorContraseña');
+
+formulario.addEventListener("submit", e => {
+    e.preventDefault();
+    let error = true;
+
+    // Limpiar mensajes de error previos
+    errorEmail.textContent = '';
+    errorContraseña.textContent = '';
+
+    // Validación del email
+    let validEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!validEmail.test(email.value)) {
+        errorEmail.textContent = 'El email no es válido';
+        error = false;
+    }
+
+    // Validación de la contraseña
+    if (contraseña.value.length < 8) {
+        errorContraseña.textContent = 'La contraseña debe tener al menos 8 caracteres';
+        error = false;
+    }
+
+
+    if (error) {
+        const formData = new FormData(formulario);
+        fetch('/iniciar_sesion', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.success) {
+                window.location.href = "/contenido";
+            } else {
+                errorContraseña.textContent = data.message; // 
+            }
+        })
+        .catch(error => console.error('Error:', error));
+    }
+});
+
+
+
