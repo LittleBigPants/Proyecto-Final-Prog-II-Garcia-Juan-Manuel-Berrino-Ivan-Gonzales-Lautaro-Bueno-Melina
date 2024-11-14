a32e1cbaa51bcfe85ae1e11bfa5cc265a1631a54
Juan Manuel Garcia <juanmanuel_garcia98@hotmail.com>
2024-11-13 21:52:02 -0300

    Mejorando js

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,6 +1,6 @@
 import { inicializarData, categories, movies } from "./api.js";
 import { inicializarCarousel } from "./carousel.js";
-import { displayCategories, displayMovies } from "./display.js";
+import { displayCategories, displayMovies, displayDescriptionMovie } from "./display.js";
 import { renderCart } from "./render-cart.js";  // Importamos la función renderCart
 import { addMovieToCart, getCart } from "./cart.js";  // Importamos la función addMovieToCart
 
@@ -16,45 +16,28 @@ document.addEventListener("DOMContentLoaded", async () => {
     const closeModal = document.querySelector(".close");
 
     let imgButton = document.querySelectorAll(".img-button");
-    imgButton.forEach((img) => {
+        imgButton.forEach((img) => {
         img.addEventListener("click", (e) => {
             if (e.currentTarget.id) {
                 const choosenMovie = movies.find((movie) => movie.id == e.currentTarget.id);
-                displayDescriptionMovie(choosenMovie);
+                displayDescriptionMovie(choosenMovie, movieDescriptionContent);
+
+                let btnAñadirCarrito = document.querySelectorAll(".btn-añadir-carrito");
+                btnAñadirCarrito.forEach((btn) => {
+                    btn.addEventListener("click", (e) => {
+                        if (e.currentTarget.id) {
+                            const choosenMovie = movies.find((movie) => movie.id == e.currentTarget.id);
+                            addMovieToCart(choosenMovie);
+                            renderCart();
+                        }
+                    });
+                });
             }
         });
     });
 
-    function displayDescriptionMovie(movie) {
-        movieDescriptionContent.innerHTML = `
-            <div class="movie-details">
-                <h1>${movie.title}</h1> 
-                <p>${movie.description}</p>
-                <img src="${movie.image_url}" alt="${movie.title}" class="img-detail">
-                <p>Fecha de estreno: <strong>${new Date(movie.release_date).toISOString().split("T")[0]}</strong></p>
-                <p>Categoría: <strong>${movie.category}</strong></p>
-                <p>Precio: <strong>${movie.price.toLocaleString("en-US", { style: "currency", currency: "USD" })}</strong></p>
-                <p>Duración: <strong>${movie.duration} minutos</strong></p>
-                <button id=${movie.id} class="btn-añadir-carrito">Añadir al carrito</button>
-            </div>
-        `;
 
-        // Mostrar el modal
-        moviePopup.style.display = "block";
-
-        let btnAñadirCarrito = document.querySelectorAll(".btn-añadir-carrito");
-        btnAñadirCarrito.forEach((btn) => {
-            btn.addEventListener("click", (e) => {
-                if (e.currentTarget.id) {
-                    const choosenMovie = movies.find((movie) => movie.id == e.currentTarget.id);
-                    addMovieToCart(choosenMovie);  // Añadimos la película al carrito
-                    renderCart();  // Volvemos a renderizar el carrito después de añadir la película
-                }
-            });
-        });
-    }
-
-    // Cerrar el modal al hacer clic en la 'x'
+        // Cerrar el modal al hacer clic en la 'x'
     closeModal.onclick = function () {
         moviePopup.style.display = "none";
     };
@@ -70,3 +53,5 @@ document.addEventListener("DOMContentLoaded", async () => {
  
 
 });
+
+
